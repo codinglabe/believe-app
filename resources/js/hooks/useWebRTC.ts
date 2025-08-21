7faf073291b430e317fba524009afe6981814375
Ashraf Uzzaman <ashraf.uzzaman04082004@gmail.com>
2025-08-22 03:22:51 +0600

    use web rtc

--- resources/js/hooks/useWebRTC.ts
+++ resources/js/hooks/useWebRTC.ts
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect, useRef, useCallback } from "react"
-import echoService from "@/services/EchoService"
+import echoService from "@/Services/EchoService"
 
 interface WebRTCHook {
   localStream: MediaStream | null
