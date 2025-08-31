c82729bfe30c0d4f3dea42a072434e0d1c7dae8c
codinglabe <condinglabe@gmail.com>
2025-09-01 01:49:16 +0600

    some delete

--- config/database.php
+++ config/database.php
@@ -65,7 +65,8 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => false,
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                //PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_KEY => env('MYSQL_ATTR_SSL_KEY'),
                 PDO::ATTR_TIMEOUT => 60,
                 PDO::ATTR_EMULATE_PREPARES => true,
             ]) : [],
